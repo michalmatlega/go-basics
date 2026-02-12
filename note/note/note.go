1c5d6b2d522c699945c0f409f347494ecb129f52
Michal Matlega <m1sz3lek@gmail.com>
2026-02-12 19:59:21 +0100

    lesson 97

---
+++ note/note/note.go
@@ -0,0 +1,45 @@
+package note
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type Note struct {
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func New(title, content string) (Note, error) {
+	if title == "" || content == "" {
+		return Note{}, errors.New("Invalid input")
+	}
+
+	return Note{
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
+	}, nil
+}
+
+func (note *Note) Display() {
+	fmt.Printf("Your note titled %v has the following content:\n\n%v", note.Title, note.Content)
+}
+
+func (note Note) Save() error {
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName) + ".json"
+
+	jsonNote, err := json.Marshal(note) //propsy musza byc publiczne by byly zapisane do jsona
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, jsonNote, 0644)
+}
