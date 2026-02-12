be6e2a8ef7ad5dc7d2349cc02352478e06c9c09d
Michal Matlega <m1sz3lek@gmail.com>
2026-02-12 11:49:06 +0100

    pointers

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -10,10 +10,10 @@ func main() {
 
 	fmt.Println("Age:", *agePointer)
 	//
-	//adultYears := getAdultYears(age)
-	//fmt.Println(adultYears)
+	getAdultYears(agePointer)
+	fmt.Println(age)
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+func getAdultYears(age *int) {
+	*age = *age - 18
 }
