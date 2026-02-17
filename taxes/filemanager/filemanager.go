a892fc0d456afb54baedeb3efe65d8c79625ffc7
Michal Matlega <m1sz3lek@gmail.com>
2026-02-17 15:44:32 +0100

    lesson 159

--- taxes/filemanager/filemanager.go
+++ taxes/filemanager/filemanager.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"os"
+	"time"
 )
 
 type FileManager struct {
@@ -19,6 +20,8 @@ func (fm FileManager) ReadLines() ([]string, error) {
 		return nil, errors.New("failed to open file")
 	}
 
+	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
 	var lines []string
 	for scanner.Scan() {
@@ -28,11 +31,11 @@ func (fm FileManager) ReadLines() ([]string, error) {
 	err = scanner.Err()
 
 	if err != nil {
-		file.Close()
+		//file.Close()
 		return nil, errors.New("failed to read lines")
 	}
 
-	file.Close()
+	//file.Close()
 	return lines, nil
 }
 
@@ -43,15 +46,19 @@ func (fm FileManager) WriteResult(data any) error {
 		return errors.New("failed to create file")
 	}
 
+	defer file.Close()
+
+	time.Sleep(3 * time.Second)
+
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data)
 
 	if err != nil {
-		file.Close()
+		//file.Close()
 		return errors.New("failed to write JSON")
 	}
 
-	file.Close()
+	//file.Close()
 	return nil
 }
 
--- taxes/main.go
+++ taxes/main.go
@@ -9,16 +9,30 @@ import (
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
+	doneChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan error, len(taxRates))
 
-	for _, taxRate := range taxRates {
-		fm := filemanager.New("pricesssss.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+	for index, taxRate := range taxRates {
+		doneChans[index] = make(chan bool)
+		errorChans[index] = make(chan error)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		//cmdm := cmdmanager.New()
 		taxIncludedPriceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		err := taxIncludedPriceJob.Process()
-		if err != nil {
-			fmt.Println("Could not process job")
-			fmt.Println(err)
-		}
+		go taxIncludedPriceJob.Process(doneChans[index], errorChans[index])
+		//if err != nil {
+		//	fmt.Println("Could not process job")
+		//	fmt.Println(err)
+		//}
 	}
 
+	for index := range taxRates {
+		select {
+		case err := <-errorChans[index]:
+			if err != nil {
+				fmt.Println(err)
+			}
+		case <-doneChans[index]:
+			fmt.Println("Done!")
+		}
+	}
 }
